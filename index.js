2bef5c2b06a880039e84a2584692071c8e90656b
ANNGLORIOUS <mueniannglorious@gmail.com>
2024-07-13 21:34:18 +0300

    first commit

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  fetch("https://anapioficeandfire.com/api/books")
+  return fetch("https://anapioficeandfire.com/api/books")
   .then((resp)=> resp.json())
-  .then((json) => console.log(json));
+  .then((books) =>renderBooks(books));
 }
 
 function renderBooks(books) {
